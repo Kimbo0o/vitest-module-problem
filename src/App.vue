27b1f8ed3404e7f2ed876e27f05380dfeac8c46a
Kimbo0o <81405223+Kimbo0o@users.noreply.github.com>
2023-08-24 11:13:25 +0200

    updated modeler mounting

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div ref="canvas" id="modeler"></div>
+  </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import {
   BpmnPropertiesPanelModule,
@@ -10,12 +13,20 @@ import {
   BpmnPropertiesProviderModule,
 } from 'bpmn-js-properties-panel';
 
+const canvas = ref();
 
-new BpmnModeler({
+const modeler = new BpmnModeler({
   additionalModules: [
     BpmnPropertiesPanelModule,
     CamundaPlatformPropertiesProviderModule,
     BpmnPropertiesProviderModule,
   ],
+});
+
+onMounted(() => {
+  console.log('mounted');
+
+  console.log(canvas.value);
+  modeler.attachTo(canvas.value);
 });
 </script>
